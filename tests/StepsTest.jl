38f59085d3bf0e856a66f4e38174627939283a04
Abraham Thomas <abt150@gmail.com>
2025-08-25 13:43:35 -0500

    minor fixes to StepToward

--- tests/StepsTest.jl
+++ tests/StepsTest.jl
@@ -1,6 +1,15 @@
 include("../src/Steps.jl")
+include("../src/Point.jl")
 
 ### Tests for StepToward ###
+stepSize = 0.7
+# remember to test step size 0, 1, and beyond! -1, 2... 
+source = Point(0.0, 0.0)
+target = Point(2.0, 1.0)
+
+step = StepToward(source, target, stepSize)
+println(step)
+
 ### End tests for StepToward ###
 
 ### Tests for ComputeStepsWithBounds ###

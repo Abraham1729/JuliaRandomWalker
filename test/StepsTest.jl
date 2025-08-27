9111713a23b8ce17267075730a169a44d5673274
Abraham1729 <77895428+Abraham1729@users.noreply.github.com>
2025-08-27 14:25:06 -0500

    tests for ComputeStepBounds

--- test/StepsTest.jl
+++ test/StepsTest.jl
@@ -1,4 +1,4 @@
-### Tests for ComputeStepToward ###
+### Begin tests for ComputeStepToward ###
 @testset verbose = true "Testing ComputeStepToward" begin
     @testset "ComputeStepToward with various step sizes" begin
         # Assemble #
@@ -26,8 +26,55 @@
         @test step == -1 * (target - source)
     end
 end
-
 ### End tests for ComputeStepToward ###
 
-# TODO: Tests for ComputeSteps
-# TODO: Tests for ComputeStepBounds
+
+### Begin tests for ComputeSteps ###
+# TODO: Learn how to mock ChooseRandomAnchor for more controlled tests
+### End tests for ComputeSteps ###
+
+
+### Begin tests for ComputeStepBounds ###
+@testset verbose=true "Testing ComputeStepBounds" begin
+    @testset "ComputeStepBounds with various step patterns" begin
+        # Test 1: Simple square pattern
+        steps = [
+            Vector2(0.0, 0.0), 
+            Vector2(1.0, 0.0), 
+            Vector2(1.0, 1.0), 
+            Vector2(0.0, 1.0)
+        ]
+        upperRight, lowerLeft = ComputeStepBounds(steps)
+        @test upperRight == Vector2(1.0, 1.0)
+        @test lowerLeft == Vector2(0.0, 0.0)
+
+        # Test 2: Negative coordinates
+        steps = [
+            Vector2(-2.0, -3.0), 
+            Vector2(-1.0, -1.0), 
+            Vector2(-4.0, -5.0)
+        ]
+        upperRight, lowerLeft = ComputeStepBounds(steps)
+        @test upperRight == Vector2(-1.0, -1.0)
+        @test lowerLeft == Vector2(-4.0, -5.0)
+
+        # Test 3: Mixed coordinates
+        steps = [
+            Vector2(-1.0, 2.0), 
+            Vector2(3.0, -4.0), 
+            Vector2(0.0, 1.0)
+        ]
+        upperRight, lowerLeft = ComputeStepBounds(steps)
+        @test upperRight == Vector2(3.0, 2.0)
+        @test lowerLeft == Vector2(-1.0, -4.0)
+
+        # Test 4: Single point
+        steps = [
+            Vector2(5.0, 5.0)
+        ]
+        upperRight, lowerLeft = ComputeStepBounds(steps)
+        @test upperRight == Vector2(5.0, 5.0)
+        @test lowerLeft == Vector2(5.0, 5.0)
+    end
+end
+### End tests for ComputeStepBounds ###

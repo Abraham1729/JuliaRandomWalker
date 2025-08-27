ccbc65fa34fe2ea658106f768b0c3d055c6a0be2
Abraham1729 <77895428+Abraham1729@users.noreply.github.com>
2025-08-27 13:57:39 -0500

    enabling setting of vector2.x and vector2.y, updating signature of compute function in steps.jl

--- src/Steps.jl
+++ src/Steps.jl
@@ -1,6 +1,3 @@
-# include("./Vector2.jl")
-# include("./Anchors.jl")
-
 function ComputeStepToward(source::Vector2, target::Vector2, stepSize::Float64)
     # Calculate direction vector
     dir_x = target.x - source.x
@@ -19,28 +16,28 @@ function ComputeStepToward(source::Vector2, target::Vector2, stepSize::Float64)
 end
 export ComputeStepToward
 
-function ComputeStepsWithBounds!(n::Int64, startLocation::Vector2)
+function ComputeStepsWithBounds(n::Int64, startLocation::Vector2, anchors:: Vector{Vector2}, stepSize::Float64 = 0.1)
     # Set up default bounds #
     upperRight = startLocation # (maxX, maxY)
     lowerLeft = startLocation  # (minX, minY)
 
     # Create our array of locations visited by our walker #
     steps = Array{Vector2}(undef, n + 1)
-    steps[0] = startLocation
+    steps[1] = startLocation
 
     # Do N steps, tracking the X/Y bounds for heatmap purposes #
-    for i in 2:length
+    for i in 2:length(steps)
         # Choose an anchor and take a step toward it #
         target = ChooseRandomAnchor(anchors)
         steps[i] = steps[i-1] += ComputeStepToward(steps[i-1], target, stepSize)
 
         # Update X bounds as needed #
-        upperRight.x < steps[i].x && upperRight.x = steps[i].x
-        lowerLeft.x > steps[i].x && lowerLeft.x = steps[i].x
+        upperRight.x < steps[i].x && (upperRight.x = steps[i].x)
+        lowerLeft.x > steps[i].x && (lowerLeft.x = steps[i].x)
 
         # Update Y bounds as needed #
-        upperRight.y < steps[i].y && upperRight.y = steps[i].y
-        lowerLeft.y > steps[i].y && lowerLeft.y = steps[i].y
+        upperRight.y < steps[i].y && (upperRight.y = steps[i].y)
+        lowerLeft.y > steps[i].y && (lowerLeft.y = steps[i].y)
     end
 
     # Returning steps + tuple of bounds, leaving to end user to unpack #
--- src/Vector2.jl
+++ src/Vector2.jl
@@ -16,6 +16,11 @@ Base.getproperty(v::Vector2, s::Symbol) =
     s === :y ? v.data[2] :
     getfield(v, s)
 
+Base.setproperty!(v::Vector2, s::Symbol, val) =
+    s === :x ? (v.data = SVector(val, v.y)) :
+    s === :y ? (v.data = SVector(v.x, val)) :
+    setfield!(v, s, val)
+
 # Equality checks #
 Base.:(==)(v1::Vector2, v2::Vector2) = v1.x == v2.x && v1.y == v2.y
 Base.isapprox(v1::Vector2, v2::Vector2; atol=1e-8) = isapprox(v1.x, v2.x, atol=atol) && isapprox(v1.y, v2.y, atol=atol)

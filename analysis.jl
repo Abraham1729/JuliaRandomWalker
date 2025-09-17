875751445cb5a13d7cf9053c986b9b3b559bde80
Abraham Thomas <abt150@gmail.com>
2025-09-17 03:31:28 -0500

    cleanup

--- analysis.jl
+++ analysis.jl
@@ -1,13 +1,12 @@
 # Start params #
+using JuliaRandomWalker
+
 # TODO: Figure out how to set a seed #
-num_steps = 10000000
+num_steps = 1000000
 num_anchors = 6
-stepSize = 1/2
+stepSize = 1 - (1/2)
 anchor_distance = 1.0
 startLocation = Vector2(0.0, 0.0)
-resX = 500
-resY = 500
-
+resX = 1000
+resY = 1000
 Run(num_steps, num_anchors, anchor_distance, startLocation, stepSize, resX, resY)
-# TODO: gks qtterm -- find out how to GUI for zooming and panning so you can show off to Henry and not look like a fool #
-
